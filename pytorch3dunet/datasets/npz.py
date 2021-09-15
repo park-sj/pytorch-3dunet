1457fd9e19f3adf8f0f3bedd25e18e9adca33c27
jwson <jwson@anymedi.com>
2021-09-15 09:53:28 +0900

    - save training data in .npz format

--- pytorch3dunet/datasets/npz.py
+++ pytorch3dunet/datasets/npz.py
@@ -57,7 +57,7 @@ class NpzDataset(ConfigDataset):
         # x = [self._load_files(os.path.join(self.file_path, self.phase, y)) for y in self.patients]
         # image_sample = self._load_files('/home/shkim/Libraries/pytorch-3dunet/datasets/JW/train/ParkHyunA/')
 #        test_patient = os.listdir(os.path.join(file_path, 'test'))
-        image_sample = np.zeros((296,296,296))
+        image_sample = np.zeros((128,128,128))
         # min_value, max_value, mean, std = calculate_stats(image_sample.astype(np.float64))
         # transformer = transforms.get_transformer(transformer_config, min_value=min_value, max_value=max_value,
         #                                          mean=mean, std=std)
@@ -73,7 +73,7 @@ class NpzDataset(ConfigDataset):
         # image_sample = downscale_local_mean(image_sample, (2,2,2))
         # image_sample = np.expand_dims(image_sample, 0)
         
-        image_sample = resize(image_sample.astype(np.float32), (296,296,296), anti_aliasing = False)
+        image_sample = resize(image_sample.astype(np.float32), (128,128,128), anti_aliasing = False)
         image_sample = np.expand_dims(image_sample, 0)
 
         slice_builder = get_slice_builder(image_sample, None, self.weight_maps, self.slice_builder_config)
@@ -91,11 +91,11 @@ class NpzDataset(ConfigDataset):
         self.cur_image = data['ct']
         
         # min_value, max_value, mean, std = calculate_stats(self.cur_image.astype(np.float32))
-        self.min_value = -750
-        self.max_value = 1250
+        self.min_value = -500
+        self.max_value = 1500
         self.cur_image[self.cur_image>self.max_value] = self.max_value
         self.cur_image[self.cur_image<self.min_value] = self.min_value
-        self.cur_image = resize(self.cur_image.astype(np.float32), (296, 296, 296), anti_aliasing = False)
+        self.cur_image = resize(self.cur_image.astype(np.float32), (128, 128, 128), anti_aliasing = False)
         mean = (self.min_value + self.max_value)/2
         std = (self.max_value - self.min_value)/2
         transformer = transforms.get_transformer(self.transformer_config, min_value=self.min_value, max_value=self.max_value,
@@ -111,7 +111,7 @@ class NpzDataset(ConfigDataset):
             #     self.cur_mask.flip(axis=2)
 #            if self.cur_mask.shape[0] >= 600:
 #                self.cur_mask = self.cur_mask[100:-100, :, :]
-            self.cur_mask = resize(self.cur_mask.astype(np.float32), (296, 296, 296), anti_aliasing = False)
+            self.cur_mask = resize(self.cur_mask.astype(np.float32), (128, 128, 128), anti_aliasing = False)
             self.cur_mask = np.expand_dims(self.cur_mask, 0)
         else:
             self.cur_mask = None
@@ -142,8 +142,8 @@ class NpzDataset(ConfigDataset):
             mask = self.label_slices[idx]
             # mask = np.expand_dims(mask, 0)
             mask = self._transform_patches(self.cur_mask, mask, self.masks_transform)
-            if self.phase == 'train':
-                image += np.random.normal(0,0.2, image.shape).astype(np.float32) # Gaussian noise
+            # if self.phase == 'train':
+            #     image += np.random.normal(0,0.2, image.shape).astype(np.float32) # Gaussian noise
                 # noise = generate_perlin_noise_3d((296,296,296), (8,8,8))*0.5
                 # noise[noise<0] = 0
                 # image += noise # Perlin noise
