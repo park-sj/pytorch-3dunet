227da142a9e2ff9d39804035e63d61cf239fb649
ANYMEDI-jwson <62132249+ANYMEDI-jwson@users.noreply.github.com>
2021-10-19 15:02:44 +0900

    - debug predictor for airway and bone

--- pytorch3dunet/datasets/ab.py
+++ pytorch3dunet/datasets/ab.py
@@ -13,13 +13,9 @@ import os
 import numpy as np
 import glob
 import SimpleITK as sitk
-import random
-from skimage.filters import laplace
-from skimage.transform import downscale_local_mean, resize
 
 import pytorch3dunet.augment.transforms as transforms
-from pytorch3dunet.datasets.perlin import generate_perlin_noise_3d
-from pytorch3dunet.datasets.utils import get_slice_builder, ConfigDataset, calculate_stats
+from pytorch3dunet.datasets.utils import ConfigDataset
 from pytorch3dunet.unet3d.utils import get_logger
 
 logger = get_logger('DicomDataset')
@@ -51,33 +47,7 @@ class ABDataset(ConfigDataset):
         self.file_path = file_path
         self.patients = os.listdir(os.path.join(file_path, phase))
         self.transformer_config = transformer_config
-        
-        
-        # x = [self._load_files(os.path.join(self.file_path, self.phase, y)) for y in self.patients]
-        # image_sample = self._load_files('/home/shkim/Libraries/pytorch-3dunet/datasets/JW/train/ParkHyunA/')
-        test_patient = os.listdir(os.path.join(file_path, 'test'))
-        image_sample = self._load_files(os.path.join(file_path, 'test', test_patient[0]))
-        # min_value, max_value, mean, std = calculate_stats(image_sample.astype(np.float64))
-        # transformer = transforms.get_transformer(transformer_config, min_value=min_value, max_value=max_value,
-        #                                          mean=mean, std=std)
-        # self.raw_transform = transformer.raw_transform()
-        # if phase != 'test':
-        #     self.masks_transform = transformer.label_transform()
-        
-        # slice
-        self.weight_maps = None
-        # image_sample = image_sample[:400, 100:500, 100:500]
-        # image_sample = image_sample[20:580, 20:580, 20:580]
-        # image_sample = image_sample[4:596, 4:596, 4:596]
-        # image_sample = downscale_local_mean(image_sample, (2,2,2))
-        # image_sample = np.expand_dims(image_sample, 0)
-        
-        image_sample = np.expand_dims(image_sample, 0)
 
-        slice_builder = get_slice_builder(image_sample, None, self.weight_maps, self.slice_builder_config)
-        image_slices = slice_builder.raw_slices
-        self.patch_per_image = len(image_slices)
-        
         self.count = -1
     
     def getImage(self, count):
@@ -105,58 +75,20 @@ class ABDataset(ConfigDataset):
             self.cur_mask = np.expand_dims(self.cur_mask, 0)
         else:
             self.cur_mask = None
-        slice_builder = get_slice_builder(self.cur_image, self.cur_mask, self.weight_maps, self.slice_builder_config)
-        self.image_slices = slice_builder.raw_slices
-        self.label_slices = slice_builder.label_slices
-
         
     def __getitem__(self, idx):
-        # self.count += 1
-        # idx = self.count
-        # if idx % self.patch_per_image == 0:
-        #     self.getImage(int(idx / self.patch_per_image))
-        
-        # self.getImage(int(idx % len(self.patients)))
-        # logger.info(f'getting item number {idx}')
-        self.getImage(int(idx / self.patch_per_image))
-        name = self.patients[int(idx / self.patch_per_image)]
-        
-        idx = idx % self.patch_per_image
-        image = self.image_slices[idx]
-        
-        # image = np.expand_dims(image, 0)
-        
-        image = self._transform_patches(self.cur_image, image, self.raw_transform)
+        self.getImage(idx)
+        name = self.patients[idx]
+        image = self.raw_transform(self.cur_image)
         
         if self.phase != 'test':
-            mask = self.label_slices[idx]
-            # mask = np.expand_dims(mask, 0)
-            mask = self._transform_patches(self.cur_mask, mask, self.masks_transform)
+            mask = self.masks_transform(self.cur_mask)
             return image, mask, name
         else:
-
-            # image = np.expand_dims(image, 0)
-            raw_idx = self.image_slices[idx]
-            if len(raw_idx) == 4:
-                raw_idx = raw_idx[1:]
-            return image, raw_idx
+            return image, name
         
     def __len__(self):
-        return len(self.patients) * self.patch_per_image
-    
-    @staticmethod
-    def _transform_patches(datasets, label_idx, transformer):
-        transformed_patches = []
-        for dataset in datasets:
-            # get the label data and apply the label transformer
-            transformed_patch = transformer(dataset[label_idx])
-            transformed_patches.append(transformed_patch)
-
-        # if transformed_patches is a singleton list return the first element only
-        if len(transformed_patches) == 1:
-            return transformed_patches[0]
-        else:
-            return transformed_patches
+        return len(self.patients)
         
     @classmethod
     def create_datasets(cls, dataset_config, phase):
--- pytorch3dunet/unet3d/predictor.py
+++ pytorch3dunet/unet3d/predictor.py
@@ -1055,7 +1055,6 @@ class ABPredictor(_AbstractPredictor):
             logger.info(f"Using only channel '{prediction_channel}' from the network output")
 
         device = self.config['device']
-        output_heads = self.config['model'].get('output_heads', 1)
 
         logger.info(f'Running prediction on {len(self.loader)} batches...')
 
@@ -1067,7 +1066,7 @@ class ABPredictor(_AbstractPredictor):
         # Run predictions on the entire input dataset
         with torch.no_grad():
             # for batch, indices in self.loader:
-            for batch, indices in self.loader:
+            for batch, name in self.loader:
                 
                 if batch.shape[0] != 1:
                     raise NotImplementedError("Currently support running only on single data")
@@ -1083,106 +1082,20 @@ class ABPredictor(_AbstractPredictor):
                 
                 batch = batch[:,:,crop[0]:crop[1], crop[2]:crop[3], crop[4]:crop[5]]
 
-                volume_shape = (batch.shape)[1:]
-        
-                if prediction_channel is None:
-                    prediction_maps_shape = (out_channels,) + volume_shape
-                else:
-                    # single channel prediction map
-                    prediction_maps_shape = (1,) + volume_shape
-        
-                logger.info(f'The shape of the output prediction maps (CDHW): {prediction_maps_shape}')
-        
-                patch_halo = self.predictor_config.get('patch_halo', (0, 0, 0))
-                self._validate_halo(patch_halo, self.config['loaders']['test']['slice_builder'])
-                logger.info(f'Using patch_halo: {patch_halo}')
-        
-                # create destination H5 file
-                # h5_output_file = h5py.File(self.output_file, 'w')
-                np_output_file = np.zeros(volume_shape)
-                # allocate prediction and normalization arrays
-                logger.info('Allocating prediction and normalization arrays...')
-                prediction_maps, normalization_masks = self._allocate_prediction_maps(prediction_maps_shape,
-                                                                                      output_heads, np_output_file)
                 # send batch to device
                 batch = torch.nn.functional.interpolate(batch, (128,128,128))
                 batch = batch.to(device)
 
                 # forward pass
                 # batch = torch.unsqueeze(batch, 0)
-                predictions = self.model(batch)
+                prediction = self.model(batch)
 
-                # x = predictions.clone().cpu()
-                # np.save('/home/shkim/Libraries/pytorch-3dunet/datasets/JW/prediction' + str(indices[0]),
-                #         x.numpy())
-                # logger.info(f'Prediciton for {indices[0]} saved as {"datasets/JW/prediction" + str(indices[0])}')
-                # wrap predictions into a list if there is only one output head from the network
-                if output_heads == 1:
-                    predictions = [predictions]
-
-                # for each output head
-                for prediction, prediction_map, normalization_mask in zip(predictions, prediction_maps,
-                                                                          normalization_masks):
-
-                    # convert to numpy array
-                    prediction = prediction.cpu().numpy()
-
-                    # for each batch sample
-                    for pred, index in zip(prediction, indices):
-                        # save patch index: (C,D,H,W)
-                        if prediction_channel is None:
-                            channel_slice = slice(0, out_channels)
-                        else:
-                            channel_slice = slice(0, 1)
-                        # print(index)
-                        index = (channel_slice,) + index
-
-                        if prediction_channel is not None:
-                            # use only the 'prediction_channel'
-                            logger.info(f"Using channel '{prediction_channel}'...")
-                            pred = np.expand_dims(pred[prediction_channel], axis=0)
-
-                        logger.info(f'Saving predictions for slice:{index}...')
-
-                        # remove halo in order to avoid block artifacts in the output probability maps
-                        u_prediction, u_index = remove_halo(pred, index, volume_shape, patch_halo)
-                        # accumulate probabilities into the output prediction array
-                        prediction_map[u_index] += u_prediction
-                        # count voxel visits for normalization
-                        normalization_mask[u_index] += 1
-
-        # save results to
-                    self._save_results(prediction_maps, normalization_masks, output_heads, np_output_file, crop)
-        # close the output H5 file
-        # h5_output_file.close()
-
-    def _allocate_prediction_maps(self, output_shape, output_heads, output_file):
-        # initialize the output prediction arrays
-        prediction_maps = [np.zeros(output_shape, dtype='float32') for _ in range(output_heads)]
-        # initialize normalization mask in order to average out probabilities of overlapping patches
-        normalization_masks = [np.zeros(output_shape, dtype='uint8') for _ in range(output_heads)]
-        return prediction_maps, normalization_masks
-
-    def _save_results(self, prediction_maps, normalization_masks, output_heads, output_file, crop):
-        def _slice_from_pad(pad):
-            if pad == 0:
-                return slice(None, None)
-            else:
-                return slice(pad, -pad)
-
-        # save probability maps
-        prediction_datasets = self._get_output_dataset_names(output_heads, prefix='predictions')
-        for prediction_map, normalization_mask, prediction_dataset in zip(prediction_maps, normalization_masks,
-                                                                          prediction_datasets):
-            prediction_map = prediction_map / normalization_mask
-
-
-            logger.info(f'Saving predictions to: {self.output_file}/{prediction_dataset}...')
-            # output_file.create_dataset(prediction_dataset, data=prediction_map, compression="gzip")
-        # np.save('/home/shkim/Libraries/pytorch-3dunet/prediction/'+prediction_dataset, prediction_map)
-        patient = os.listdir(os.path.join(os.getcwd(), 'io', 'test'))
-        self._save_dicom(prediction_map, os.path.join(os.getcwd(), 'io', 'save', patient[self.it]), crop)
-        self.it += 1
+                # convert to numpy array
+                prediction = prediction.cpu().numpy()
+                
+                # save results to
+                output_dir = self.config['loaders']['output_dir']
+                self._save_dicom(prediction, os.path.join(output_dir, name), crop)
 
     @staticmethod
     def _save_dicom(newArray, filepath, crop):
@@ -1221,8 +1134,8 @@ class ABPredictor(_AbstractPredictor):
         paddedArray[crop[0]:crop[1],crop[2]:crop[3],crop[4]:crop[5]] = newArray
         
         newImage = sitk.GetImageFromArray(paddedArray)
-
-        newImage.CopyInformation(oldImage)
+        newImage = sitk.Cast(newImage, sitk.sitkInt8)
+        # newImage.CopyInformation(oldImage)
         writer = sitk.ImageFileWriter()
         writer.KeepOriginalImageUIDOn()
         sp_x, sp_y = reader.GetMetaData(0, "0028|0030").split('\\')
@@ -1240,11 +1153,9 @@ class ABPredictor(_AbstractPredictor):
         series_tag_values = [(k, reader.GetMetaData(0, k)) for k in reader.GetMetaDataKeys(0)] + \
                              [("0008|0031", modification_time),
                              ("0008|0021", modification_date),
-                             ("0028|0010", "296"),
-                             ("0028|0011", "296"),
-                             ("0028|0100", "16"),
-                             ("0028|0101", "16"),
-                             ("0028|0102", "15"),
+                             ("0028|0100", "8"),
+                             ("0028|0101", "8"),
+                             ("0028|0102", "7"),
                              ("0028|0103", "1"),
                              ("0028|0002", "1"),
                              ("0008|0008", "DERIVED\\SECONDARY"),
