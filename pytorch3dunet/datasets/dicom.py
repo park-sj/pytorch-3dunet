0a6acf640452a762e6a268a700cb0d8b92698b1e
ANYMEDI-jwson <jwson@anymedi.com>
2021-12-30 15:52:28 +0900

    - Config yaml controls preprocessing

--- pytorch3dunet/datasets/dicom.py
+++ pytorch3dunet/datasets/dicom.py
@@ -51,33 +51,7 @@ class DicomDataset(ConfigDataset):
         self.patients = os.listdir(os.path.join(file_path, phase))
         self.transformer_config = transformer_config
         
-        
-        # x = [self._load_files(os.path.join(self.file_path, self.phase, y)) for y in self.patients]
-        # image_sample = self._load_files('/home/shkim/Libraries/pytorch-3dunet/datasets/JW/train/ParkHyunA/')
-        test_patient = os.listdir(os.path.join(file_path, 'test'))
-        image_sample = self._load_files(os.path.join(file_path, 'test', test_patient[0]))
-        # min_value, max_value, mean, std = calculate_stats(image_sample.astype(np.float64))
-        # transformer = transforms.get_transformer(transformer_config, min_value=min_value, max_value=max_value,
-        #                                          mean=mean, std=std)
-        # self.raw_transform = transformer.raw_transform()
-        # if phase != 'test':
-        #     self.masks_transform = transformer.label_transform()
-        
-        # slice
-        self.weight_maps = None
-        # image_sample = image_sample[:400, 100:500, 100:500]
-        # image_sample = image_sample[20:580, 20:580, 20:580]
-        # image_sample = image_sample[4:596, 4:596, 4:596]
-        # image_sample = downscale_local_mean(image_sample, (2,2,2))
-        # image_sample = np.expand_dims(image_sample, 0)
-        
-        image_sample = resize(image_sample.astype(np.float32), (296,296,296), anti_aliasing = False)
-        image_sample = np.expand_dims(image_sample, 0)
-
-        slice_builder = get_slice_builder(image_sample, None, self.weight_maps, self.slice_builder_config)
-        image_slices = slice_builder.raw_slices
-        self.patch_per_image = len(image_slices)
-        
+        self.patch_per_image = 1
         self.count = -1
     
     def getImage(self, count):
@@ -86,80 +60,42 @@ class DicomDataset(ConfigDataset):
         if self.phase == 'test':
             logger.info(f'Loading dcm files from {os.path.join(self.file_path, self.phase, self.patients[count])}')
         self.cur_image = self._load_files(os.path.join(self.file_path, self.phase, self.patients[count]))
+        
+        # TODO: replace this as transform
         if self.phase != 'test' and self.cur_image.shape[0]>=600:
             self.cur_image = self.cur_image[100:-100, :, :]
-        # self.cur_image = self.cur_image[20:580, 20:580, 20:580]
-        # self.cur_image = self.cur_image[4:596, 4:596, 4:596]
-        self.flip = False
-        if random.getrandbits(1):
-            self.flip = True
-            self.cur_image = self.cur_image[:,:,::-1]
+        # TODO: make transform for flip for training (augmentation)
         
-        # min_value, max_value, mean, std = calculate_stats(self.cur_image.astype(np.float32))
-        self.min_value = -750
-        self.max_value = 1250
-        self.cur_image[self.cur_image>self.max_value] = self.max_value
-        self.cur_image[self.cur_image<self.min_value] = self.min_value
-        self.cur_image = resize(self.cur_image.astype(np.float32), (296, 296, 296), anti_aliasing = False)
-        mean = (self.min_value + self.max_value)/2
-        std = (self.max_value - self.min_value)/2
-        transformer = transforms.get_transformer(self.transformer_config, min_value=self.min_value, max_value=self.max_value,
-                                                 mean=mean, std=std)
+        # The input arguments are dummy values
+        transformer = transforms.get_transformer(self.transformer_config, min_value=0, max_value=0,
+                                                 mean=0, std=0)
         self.raw_transform = transformer.raw_transform()
         if self.phase != 'test':
             self.masks_transform = transformer.label_transform()
-        self.cur_image = np.expand_dims(self.cur_image, 0)        
+            
         if self.phase != 'test':
             self.cur_mask = self._load_files(os.path.join(self.file_path, self.phase + '_masks', self.patients[count]))
-            if self.flip:
-            #     self.cur_image = self.cur_image[:,::-1,:]
-                self.cur_mask = self.cur_mask[:,:,::-1]
             if self.cur_mask.shape[0] >= 600:
                 self.cur_mask = self.cur_mask[100:-100, :, :]
-            self.cur_mask = resize(self.cur_mask.astype(np.float32), (296, 296, 296), anti_aliasing = False)
-            self.cur_mask = np.expand_dims(self.cur_mask, 0)
         else:
             self.cur_mask = None
-        slice_builder = get_slice_builder(self.cur_image, self.cur_mask, self.weight_maps, self.slice_builder_config)
-        self.image_slices = slice_builder.raw_slices
-        self.label_slices = slice_builder.label_slices
 
         
     def __getitem__(self, idx):
-        # self.count += 1
-        # idx = self.count
-        # if idx % self.patch_per_image == 0:
-        #     self.getImage(int(idx / self.patch_per_image))
-        
-        # self.getImage(int(idx % len(self.patients)))
-        # logger.info(f'getting item number {idx}')
         self.getImage(int(idx / self.patch_per_image))
         name = self.patients[int(idx / self.patch_per_image)]
         
         idx = idx % self.patch_per_image
-        image = self.image_slices[idx]
-        
-        # image = np.expand_dims(image, 0)
-        
-        image = self._transform_patches(self.cur_image, image, self.raw_transform)
+        image = self.raw_transform(self.cur_image)
         
         if self.phase != 'test':
-            mask = self.label_slices[idx]
-            # mask = np.expand_dims(mask, 0)
-            mask = self._transform_patches(self.cur_mask, mask, self.masks_transform)
+            mask = self.masks_transform(self.cur_mask)
+            # TODO: Replace this by transform
             if self.phase == 'train':
                 image += np.random.normal(0,0.2, image.shape).astype(np.float32) # Gaussian noise
-                # noise = generate_perlin_noise_3d((296,296,296), (8,8,8))*0.5
-                # noise[noise<0] = 0
-                # image += noise # Perlin noise
             return image, mask, name
         else:
-
-            # image = np.expand_dims(image, 0)
-            raw_idx = self.image_slices[idx]
-            if len(raw_idx) == 4:
-                raw_idx = raw_idx[1:]
-            return image, raw_idx
+            return image
         
     def __len__(self):
         return len(self.patients) * self.patch_per_image
